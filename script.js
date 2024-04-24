85f70b5c73a00208ce98de363882e2d8ff24e8db
alejandro <ng.alejandro.12@gmail.com>
2024-04-24 20:01:14 +0200

    Started to change rps game to create a UI for it

--- script.js
+++ script.js
@@ -1,46 +1,71 @@
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
 const rockWin = "Rock beats Scissors";
 const paperWin = "Paper beats Rock";
 const scissorsWin = "Scissors beats paper";
-const rock = "rock";
-const paper = "paper";
-const scissors = "scissors";
+
 let gamesCount = 0;
+let playerWins = 0;
+let computerWins = 0;
+
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const div = document.querySelector("#result");
+
+rockBtn.addEventListener("click",() => playOneRound(ROCK,getComputerChoice()));
+paperBtn.addEventListener("click",() =>playOneRound(PAPER,getComputerChoice()));
+scissorsBtn.addEventListener("click",() =>playOneRound(SCISSORS,getComputerChoice()));
+
 function getComputerChoice(){
     let value = Math.floor(Math.random() * 3);
-    console.log(value);
     if (value === 0) return "rock";
     else if (value === 1) return "paper";
     else if (value === 2) return "scissors";
 }
+
 function playOneRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    if(playerSelection === rock && computerSelection === rock){
-        return "You have tied!";
+    if(playerSelection === ROCK && computerSelection === ROCK){
+        div.textContent = "You have tied!";
     }
-    else if(playerSelection === rock && computerSelection === paper){
-        return "You lose! ".concat(paperWin);
+    else if(playerSelection === ROCK && computerSelection === PAPER){
+        div.textContent = "You lose! ".concat(paperWin);
+        computerWins++;
     }
-    else if(playerSelection === rock && computerSelection === scissors){
-        return "You win! ".concat(rockWin);
+    else if(playerSelection === ROCK && computerSelection === SCISSORS){
+        div.textContent = "You win! ".concat(rockWin);
+        playerWins++;
     }
-    else if(playerSelection === paper && computerSelection === rock){
-        return "You win! ".concat(paperWin);
+    else if(playerSelection === PAPER && computerSelection === ROCK){
+        div.textContent = "You win! ".concat(paperWin);
+        playerWins++;
     }
-    else if(playerSelection === paper && computerSelection === paper){
-        return "You have tied!";
+    else if(playerSelection === PAPER && computerSelection === PAPER){
+        div.textContent = "You have tied!";
     }
-    else if(playerSelection === paper && computerSelection === scissors){
-        return "You lose! ".concat(rockWin);
+    else if(playerSelection === PAPER && computerSelection === SCISSORS){
+        div.textContent = "You lose! ".concat(rockWin);
+        computerWins++;
     }
-    else if(playerSelection === scissors && computerSelection === rock){
-        return "You lose! ".concat(rockWin);
+    else if(playerSelection === SCISSORS && computerSelection === ROCK){
+        div.textContent = "You lose! ".concat(rockWin);
+        computerWins++;
     }
-    else if(playerSelection === scissors && computerSelection === paper){
-        return "You win! ".concat(scissorsWin);
+    else if(playerSelection === SCISSORS && computerSelection === PAPER){
+        div.textContent = "You win! ".concat(scissorsWin);
+        playerWins++;
+        
     }
-    else if(playerSelection === scissors && computerSelection === scissors){
-        return "You have tied!";
+    else if(playerSelection === SCISSORS && computerSelection === SCISSORS){
+        div.textContent = "You have tied!";
     }
+    gamesCount++;
+    if (gamesCount === 5) {
+
+    }
+
     
 }
 function playGame(){
@@ -54,6 +79,7 @@ function playGame(){
         if (gamesCount === 5) askPlayAgain(); 
     }
 }
+
 function askPlayAgain(){
     let response = prompt("Do you want to play again? (Yes/No)");
     response = response.toLowerCase();
@@ -61,4 +87,3 @@ function askPlayAgain(){
 }
 //console.log(computerSelection);
 //console.log(playOneRound(playerSelection,computerSelection));
-playGame();
